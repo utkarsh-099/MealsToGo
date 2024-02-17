6dc1f689e995cef0106c6c03cc7c5be2059bba27
Utkarsh <utkarshpandit099@gmail.com>
2024-02-17 20:57:07 +0530

    Add react-native and metro config

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,16 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>
-        Our ReactNative Blank Canvas
-      </Text>
-    </View>
-  );
+    <>
+      <SafeAreaView style={{flex: 1, marginTop: StatusBar.currentHeight}}>
+        <View style={{padding: 16, backgroundColor: "green"}}>
+          <Text>Search</Text>
+        </View>
+        <View style={{flex: 1, padding: 16, backgroundColor: "blue"}}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
+  );   
 }
 
 const styles = StyleSheet.create({
-  
 });
